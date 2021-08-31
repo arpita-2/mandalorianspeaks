79926adb3e5434b481a7d5a1b67e5ba384d4dcf5
Arpita Dasgupta <dasguptaarpita1999@gmail.com>
2021-09-01 03:53:21 +0530

    error handler

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ var buttonTranslate = document.querySelector("#button-translate");
 buttonTranslate.addEventListener("click", buttonClickHandler)
 
 var translateOutput = document.querySelector("#translate-output");
+
+var serverurl = "https://api.funtranslations.com/translate/mandalorian.json"
+
+function getTranslationURL(input){
+    return serverurl+"?"+"text= "+input  
+}
+
+function errorHandler(error)
+{console.log("Error Occured!");
+alert("Something went wrong! please try again");
+}
+	
